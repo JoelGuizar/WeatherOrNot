a2c7e1849423228137a00f72434abe448e402810
Joel Guizar <joelguizar@gmail.com>
2017-08-22 02:07:03 -0700

    clean up pre API call

--- geocode/geocode.js
+++ geocode/geocode.js
@@ -11,13 +11,16 @@ const geocodeAddress = (address, cb) => {
     //console.log(JSON.stringify(body, undefined, 2)); //third argument is how you wanna format the json//the spaces
 
     if (err){
-      cb('unable to connect')
+      cb('Unable to connect.')
     } else if (body.status === 'ZERO_RESULTS'){
-      cb('unable to find that address')
+      cb('Unable to find that address.')
     } else if (body.status === 'OK'){
       let loc = body.results[0].geometry.location
-      console.log(`Address: ${body.results[0].formatted_address}`);
-      console.log(`Latitude, Longitude: ${loc.lat}, ${loc.lng}`);
+      cb(undefined, {
+        address: body.results[0].formatted_address,
+        lattitude: loc.lat,
+        longitude: loc.lng
+      })
     }
   })
 }
